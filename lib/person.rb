83e901182f91e6929a1a9ddbdff1b11f198b2ab0
Christopher Osborn <z68ozz@gmail.com>
2020-05-18 05:21:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,8 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(hash=nil)
-  if 
+  if hash 
+    hash.each |k,v|
+    
   
 end
